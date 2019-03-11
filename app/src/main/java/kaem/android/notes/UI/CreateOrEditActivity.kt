d7e6d404ed39bf42f912c9026aa53466420e7bd1
kevynbct <kevyn.buchet@gmail.com>
2019-03-11 20:12:22 +0100

    ajout connection api

--- app/src/main/java/kaem/android/notes/UI/CreateOrEditActivity.kt
+++ app/src/main/java/kaem/android/notes/UI/CreateOrEditActivity.kt
@@ -2,12 +2,31 @@ package kaem.android.notes.UI
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import kaem.android.notes.R
+import kaem.android.notes.Utils.APIClient
 
 class CreateOrEditActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_or_edit)
+    }
+
+    fun testButton(v : View){
+        val textView = findViewById<TextView>(R.id.textViewTest)
+
+        Thread {
+            val testVerse = APIClient.getVerse("gen", 1, 1, null)
+
+            runOnUiThread {
+                //textView.text = testVerse
+                textView.text = Html.fromHtml(testVerse)
+                Toast.makeText(applicationContext, testVerse, Toast.LENGTH_SHORT).show()
+            }
+        }.start()
     }
 }
--- app/src/main/java/kaem/android/notes/Utils/APIClient.kt
+++ app/src/main/java/kaem/android/notes/Utils/APIClient.kt
@@ -1,4 +1,29 @@
 package kaem.android.notes.Utils
 
+import android.util.Log
+import okhttp3.*
+import java.io.IOException
+import java.lang.Exception
+import android.R.string
+import android.os.AsyncTask.execute
+
+
+
 class APIClient {
+
+    companion object {
+        private val client = OkHttpClient()
+        private val apiAdress = "http://ibibles.net/quote.php?lsg-"
+
+        fun getVerse(book: String, chapter: Int, startVerse: Int, endVerse: Int?) : String? {
+
+            val request = Request.Builder()
+                //.url("$apiAdress-$book/$chapter:$startVerse")
+                .url("http://ibibles.net/quote.php?lsg-gen/1:1")
+                .build()
+
+            client.newCall(request).execute().use { response -> return response.body()?.string() }
+
+        }
+    }
 }
