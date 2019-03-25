4823e1350e9ca1fd0bf5401ecb8e5daf0644a976
kevynbct <kevyn.buchet@gmail.com>
2019-03-25 20:23:59 +0100

    Création de l'activity à propos

---
+++ app/src/main/java/kaem/android/notes/ui/AboutActivity.kt
@@ -0,0 +1,13 @@
+package kaem.android.notes.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kaem.android.notes.R
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
